5fff0e9450d551e54f1eb7e409845fb32dd6531e
Analiza Jech <2101010055@undc.edu.pe>
2025-03-24 15:06:32 -0500

    first commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,50 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import {
+  ApiOperation,
+  ApiParam,
+  ApiBody,
+  ApiResponse,
+  ApiTags,
+ } from '@nestjs/swagger';
+
+@ApiTags('Products api controller')
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @ApiOperation({ summary: 'Crea productos' })
+  @ApiResponse({
+  status: 200,
+  description: 'Producto Nuevo',
+  type: [CreateProductDto],
+  })
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(+id, updateProductDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
+}
